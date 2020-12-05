cf140907f3ceda59398866457f450c5718f7e102
PC Information <altaffaisal60@gmail.comcd>
2020-12-05 12:45:20 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+  <link rel="stylesheet" href="/css/bootstrap-theme.min.css" />
+  <link rel="stylesheet" href="/fontawesome-free-5.15.1-web/css/all.min.css">
+
+  <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@200&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="/style.css" />
+  <title>POFO – Creative Agency, Corporate and Portfolio</title>
+</head>
+
+<body>
+  <!-- Navbar  -->
+  <section>
+    <nav class="navbar navbar-expand-lg fixed-top">
+      <a class="navbar-brand" href="#"><img src="/img/logo.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="#navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="container">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto mx-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Page <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Portfolio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Blog<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Element <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              <i class="fas fa-search"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </section>
+  <div class="background-image">
+    <div class="intro img">
+            <h2>Be Branding</h2>
+      <p>Lorem Ipsum is simply dummy text of the printing</p>
+      <button type="button" class="btn btn-danger btn-lg">DOWNLOAD NOW</button>
+      <div class="mouse">
+        <!-- <img src="/img/Untitled-12.png" alt="#"> -->
+        <!-- <video src="/img/Polygon - 6824.mp4"></video> -->
+      </div>
+    </div>
+  </div>
+  <div class="second-2">
+  <div class="container">
+    <div class="row my-4">
+      <div class="col-md-4">
+        <div class="card mb-4">
+          <p class="p-heading my-4 mx-4">EASY WAY TO BUILD WEBSITES</p>
+
+          <div class="card-body">
+            <div class="h2-heading">
+              <h2>Perfect powerful theme for creatives designer people.</h2>
+            </div>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+              <button type="button" class="btn btn-secondary btn-lg">
+                Large button
+              </button>
+            </div>
+          </div>
+        </div>
+      </div></div>
+      <div class="col-md-4">
+        <div class="card mb-4 ">
+          <img src="/img/homepage-option15-image-1.png" alt="">
+          <div class="card-body">
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mb-4 ">
+          <img src="/img/homepage-option15-image-2.png" alt="">
+          <div class="card-body">
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- 3thid work  -->
+  <div class="third-work">
+    <div class="container  d-flex justify-content-around align-items-center text-center">
+      <div class="heading">
+
+        <i class="fas fa-cut fa-rotate-270"></i>
+        <div class="icons">
+
+          <h2>Live website builder</h2>
+        </div>
+      </div>
+
+
+
+      <div class="heading">
+
+        <i class="fas fa-cut"></i>
+        <div class="icons">
+
+          <h2>Live website builder</h2>
+        </div>
+      </div>
+      <div class="heading">
+
+        <i class="fas fa-cut"></i>
+        <div class="icons">
+
+          <h2>Live website builder</h2>
+        </div>
+      </div>
+      <div class="heading">
+
+        <i class="fas fa-cut"></i>
+        <div class="icons">
+
+          <h2>Live website builder</h2>
+        </div>
+      </div>
+      <div class="heading">
+
+        <i class="fas fa-cut"></i>
+        <div class="icons">
+
+          <h2>Live website builder</h2>
+        </div>
+      </div>
+      <div class="heading">
+
+        <i class="fas fa-cut "></i>
+        <div class="icons">
+
+          <h2>Live website builder</h2>
+        </div>
+      </div>
+
+    </div>
+  </div>
+  <!-- fourth part  -->
+
+  <div class="bottom-heading">
+
+    <div class="container">
+      <div class="top-writing">
+        <h2>Beautiful and easy to understand UI, professional animations and drag & drop</h2>
+      </div>
+      <div class="fourth-p"><p>years of experience in the website design and development industry ThemeZaa pride
+        ourselves on creating unique, creative and quality designs that are developed upon the latest modern coding and
+        developing techniques, which are then built using the most up to date, structured coding framework so that your
+        development team can take it to the next level with ease.</p> </div>
+      <img src="/img/homepage-option15-image-3.png" alt="">
+    </div>
+    <div class="fourth-h2"> </div>
+  </div>
+
+  <div class="OUR-RECENT-WORKS">
+    <div class="container">
+      <p>OUR RECENT WORKS</p>
+      <div class="our-h2"> 
+        <h2>
+          New stunning projects  for our amazing clients
+        </h2>
+      </div>
+    </div>
+  </div> 
+<!-- grid   -->
+<div class="grid-system mx-2">
+
+  <div class="row">
+    <div class="col-2 mx-0"><img src="/img/1 (1).jpg" alt=""></div>
+    <div class="col-2"><img src="/img/1 (1).jpg" alt=""></div>
+    <div class="col-4 mx-0"><img src="/img/1 (1).jpg" alt=""></div>
+    <div class="col-4"><img src="/img/1 (1).jpg" alt=""></div>
+  </div>
+  <div class="row ">
+    <div class="col-4 "><img src="/img/1 (7).jpg" alt=""></div>
+    <div class="col-4 "> 
+      <img src="/img/1 (4).jpg" alt="">
+    </div>
+  </div>
+</div>
+<div class="container-xl"><p> wide until small breakpoint</p></div>
+<!-- Optional JavaScript; choose one of the two! -->
+
+
+  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+    crossorigin="anonymous"></script>
+
+  <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
